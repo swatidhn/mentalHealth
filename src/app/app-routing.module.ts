82b1ba285cc1a5e8f1c12a7e4f4c8f50ae4066da
swatidhn <swatineelamsinha@gmail.com>
2024-06-09 16:45:08 +0530

    new homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent, pathMatch: 'full' },{ path: 'stories', loadChildren: () => import('./stories/stories.module').then(m => m.StoriesModule) }, { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) }, { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }, { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) }, { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }];
+const routes: Routes = [{ path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: 'stories', loadChildren: () => import('./stories/stories.module').then(m => m.StoriesModule) },
+ { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) },
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
